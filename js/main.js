6549e6c961d38402fadda80309327bb23859ba5a
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-01 00:47:45 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+console.log("My Enemies List!")
+console.log("----------------")
+
+let enemies = []
+
+for (let myEnemy of enemies) {
+    if (myEnemy.isReallyHated) {
+        console.log(`${myEnemy.firstName} ${myEnemy.lastName} (Really, really dislike!)`);
+    }
+    else {
+        console.log(`${myEnemy.firstName} ${myEnemy.lastName}`);
+    }
+}
+
+const GetEnemies = () => {
+    enemies = [
+        {
+            firstName: "Joshua",
+            lastName: "Flowers",
+            offenses: ["Being a jerk to me in elementary school",
+                "Not being nice to me in elementary school"],
+            isReallyHated: true
+        },
+        {
+            firstName: "Darth",
+            lastName: "Vader",
+            offenses: ["Cut off Luke's hand",
+                "Murdered all those kids",
+                "Unkind management practices"],
+            isReallyHated: false
+        },
+        {
+            firstName: "Betty",
+            lastName: "Rudelady",
+            offenses: ["Phone calls in the theater",
+                "Phone calls on the bus",
+                "Phone calls in line at the grocery store",
+                "Poor conversationalist"],
+            isReallyHated: true
+        },
+        {
+            firstName: "Leon",
+            lastName: "Peck",
+            offenses: ["Keeps giving me a hotplate"],
+            isReallyHated: false
+        }
+    ]
+    return enemies
+}
+
+
