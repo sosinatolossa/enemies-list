a7e6429ceeb4c802be5150f21c6545efe48e0f85
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-01 09:17:45 -0600

    c# & js code have same output now

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
 console.log("My Enemies List!")
 console.log("----------------")
 
-let enemies = []
+
+const enemies = [
+    {
+        firstName: "Joshua",
+        lastName: "Flowers",
+        offenses: ["Being a jerk to me in elementary school",
+            "Not being nice to me in elementary school"],
+        isReallyHated: true
+    },
+    {
+        firstName: "Darth",
+        lastName: "Vader",
+        offenses: ["Cut off Luke's hand",
+            "Murdered all those kids",
+            "Unkind management practices"],
+        isReallyHated: false
+    },
+    {
+        firstName: "Betty",
+        lastName: "Rudelady",
+        offenses: ["Phone calls in the theater",
+            "Phone calls on the bus",
+            "Phone calls in line at the grocery store",
+            "Poor conversationalist"],
+        isReallyHated: true
+    },
+    {
+        firstName: "Leon",
+        lastName: "Peck",
+        offenses: ["Keeps giving me a hotplate"],
+        isReallyHated: false
+    }
+]
+
 
 for (let myEnemy of enemies) {
     if (myEnemy.isReallyHated) {
@@ -10,42 +43,6 @@ for (let myEnemy of enemies) {
     else {
         console.log(`${myEnemy.firstName} ${myEnemy.lastName}`);
     }
-}
-
-const GetEnemies = () => {
-    enemies = [
-        {
-            firstName: "Joshua",
-            lastName: "Flowers",
-            offenses: ["Being a jerk to me in elementary school",
-                "Not being nice to me in elementary school"],
-            isReallyHated: true
-        },
-        {
-            firstName: "Darth",
-            lastName: "Vader",
-            offenses: ["Cut off Luke's hand",
-                "Murdered all those kids",
-                "Unkind management practices"],
-            isReallyHated: false
-        },
-        {
-            firstName: "Betty",
-            lastName: "Rudelady",
-            offenses: ["Phone calls in the theater",
-                "Phone calls on the bus",
-                "Phone calls in line at the grocery store",
-                "Poor conversationalist"],
-            isReallyHated: true
-        },
-        {
-            firstName: "Leon",
-            lastName: "Peck",
-            offenses: ["Keeps giving me a hotplate"],
-            isReallyHated: false
-        }
-    ]
-    return enemies
 }
 
 
